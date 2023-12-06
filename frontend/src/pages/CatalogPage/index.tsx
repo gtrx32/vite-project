840e8fa5f52e818e6b5e87fb6ad3bd6937737e48
danil348 <eliseevdanil348@gmail.com>
2023-12-06 19:36:13 +0300

    #style - catalog types

--- frontend/src/pages/CatalogPage/index.tsx
+++ frontend/src/pages/CatalogPage/index.tsx
@@ -1,22 +1,7 @@
 import { useState, useEffect } from 'react';
 import MainContainer from '../../components/MainContainer';
 import s from './CatalogPage.module.scss';
-
-interface CatalogPageProps {}
-
-export type Game = {
-  id: number;
-  name: string;
-  date: string;
-  creator: string;
-  platform: string;
-  imageUrl: string;
-  genres: string;
-  description: string;
-  images: string;
-  created_at: string;
-  updated_at: string;
-};
+import { CatalogPageProps, Game } from './types';
 
 const CatalogPage: React.FC<CatalogPageProps> = () => {
   const [data, setData] = useState<Game[]>([]);
---
+++ frontend/src/pages/CatalogPage/types.ts
@@ -0,0 +1,15 @@
+export interface CatalogPageProps {}
+
+export type Game = {
+  id: number;
+  name: string;
+  date: string;
+  creator: string;
+  platform: string;
+  imageUrl: string;
+  genres: string;
+  description: string;
+  images: string;
+  created_at: string;
+  updated_at: string;
+};
