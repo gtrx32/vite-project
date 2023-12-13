1fda2a15ed72cf3db2dcd9cdf8eb200f542809c7
danil348 <eliseevdanil348@gmail.com>
2023-12-14 01:07:40 +0300

    #add #frontend - route to game page

--- frontend/src/pages/CatalogPage/index.tsx
+++ frontend/src/pages/CatalogPage/index.tsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react';
 import MainContainer from '../../components/MainContainer';
 import s from './CatalogPage.module.scss';
 import { CatalogPageProps, GameType } from './types';
+import { useNavigate } from 'react-router-dom';
 
 const CatalogPage: React.FC<CatalogPageProps> = () => {
   const [data, setData] = useState<GameType[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,11 +18,13 @@ const CatalogPage: React.FC<CatalogPageProps> = () => {
     fetchData();
   }, []);
 
+  const onHandleClick = (gameId: number) => navigate(`/game/${gameId}`);
+
   return (
     <MainContainer>
       <div className={s.catalog}>
         {data.map(({ id, name, imageUrl, platform }) => (
-          <div key={id} className={s.game}>
+          <div key={id} className={s.game} onClick={() => onHandleClick(id)}>
             <div className={s['game__image']}>
               <img loading='lazy' src={imageUrl} alt='' />
             </div>
