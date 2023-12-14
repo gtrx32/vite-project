2c859630da27538b64bf633710143f3534440ca0
danil348 <eliseevdanil348@gmail.com>
2023-12-14 03:36:21 +0300

    #add #frontend - fetch request

--- frontend/src/pages/ReviewsPage/index.tsx
+++ frontend/src/pages/ReviewsPage/index.tsx
@@ -1,15 +1,37 @@
+import { useState, useEffect } from 'react';
 import MainContainer from '../../components/MainContainer';
+import { ReviewType } from './types';
+
 import s from './ReviewsPage.module.scss';
 
-interface ReviewsPageProps {
-}
+const ReviewsPage = () => {
+  const [data, setData] = useState<ReviewType[]>([]);
 
-const ReviewsPage: React.FC<ReviewsPageProps> = () => {
+  console.log(data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://127.0.0.1:8000/api/reviews');
+      const data = await response.json();
+      setData(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <MainContainer>
-      <div>reviews</div>
+      <div className={s.reviews}>
+        {data.map(({ id, name, description, date }) => (
+          <div key={id} className={s.review}>
+            <div className={s['review__name']}>{name}</div>
+            <div className={s['review__text']}>{description}</div>
+            <div className={s['review__date']}>{date}</div>
+          </div>
+        ))}
+      </div>
     </MainContainer>
-  )
+  );
 };
 
 export default ReviewsPage;
---
+++ frontend/src/pages/ReviewsPage/types.ts
@@ -0,0 +1,9 @@
+export type ReviewType = {	
+	id: number
+	name: string
+	imageUrl: string
+	description: string
+	date: string
+	created_at: string
+	updated_at: string
+};
