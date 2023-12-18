c44388caaec8fe01d06d1ced593b4e87905c975c
danil348 <eliseevdanil348@gmail.com>
2023-12-18 23:41:33 +0300

    #change #frontend - changing the localStorage to cookies

--- frontend/src/context/ContextProvider.tsx
+++ frontend/src/context/ContextProvider.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createContext, useContext, useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 const StateContext = createContext({
   user: {
@@ -17,20 +18,25 @@ const StateContext = createContext({
 });
 
 export const ContextProvider = ({ children }: any) => {
+  const [cookies, setCookie, removeCookie] = useCookies(['ACCESS_TOKEN']);
   const [user, setUser] = useState({
     name: '',
     email: '',
     avatar: '',
   });
-  const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN') ?? '');
+  const [token, _setToken] = useState(cookies.ACCESS_TOKEN ?? '');
   const [notification, _setNotification] = useState('');
 
   const setToken = (token: string) => {
     _setToken(token);
     if (token) {
-      localStorage.setItem('ACCESS_TOKEN', token);
+
+      /**
+       * Set Cookie with 60 * 60 * 24 = 86400 seconds = 1 day
+      */
+      setCookie('ACCESS_TOKEN', token, { path: '/', maxAge: 60 * 60 * 24 });
     } else {
-      localStorage.removeItem('ACCESS_TOKEN');
+      removeCookie('ACCESS_TOKEN');
     }
   };
 
