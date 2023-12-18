766af5602617e35e08341589796938a202340f66
danil348 <eliseevdanil348@gmail.com>
2023-12-19 00:34:25 +0300

    #add #frontend - error handle

--- frontend/src/pages/ProfilePage/index.tsx
+++ frontend/src/pages/ProfilePage/index.tsx
@@ -6,24 +6,36 @@ import { axiosClient } from '../../api/client';
 import s from './ProfilePage.module.scss';
 
 const ProfilePage = () => {
-  const { user, setAvatar, setPhone } = useStateContext();
   document.title = 'Профиль';
+  const { user, setAvatar, setPhone } = useStateContext();
   const [avatarValue, setAvatarValue] = useState('');
   const [phoneValue, setPhoneValue] = useState('');
+  const [phoneError, setPhoneError] = useState('');
+  const [avatarError, setAvatarError] = useState('');
 
   const onHandleChangeAvatar = async () => {
     if (user?.email) {
-      axiosClient.post('/userAvatar', { email: user?.email, avatar: avatarValue }).then(() => {
-        setAvatar(avatarValue);
-      });
+      axiosClient
+        .post('/userAvatar', { email: user?.email, avatar: avatarValue })
+        .then(() => {
+          setAvatar(avatarValue);
+        })
+        .catch(error => {
+          setAvatarError(error.response.data.message);
+        });
     }
   };
 
   const onHandleChangePhone = async () => {
     if (user?.email) {
-      axiosClient.post('/userPhone', { email: user?.email, phone: phoneValue }).then(() => {
-        setPhone(phoneValue);
-      });
+      axiosClient
+        .post('/userPhone', { email: user?.email, phone: phoneValue })
+        .then(() => {
+          setPhone(phoneValue);
+        })
+        .catch(error => {
+          setPhoneError(error.response.data.message);
+        });
     }
   };
 
@@ -34,11 +46,13 @@ const ProfilePage = () => {
       <div>
         <input type='text' placeholder='avatar' onChange={event => setAvatarValue(event.target.value)} value={avatarValue} />
         <button onClick={onHandleChangeAvatar}>update avatar</button>
+        {avatarError && <div className={s.error}>{avatarError}</div>}
         <img loading='lazy' src={user?.avatar} width='200' alt='' />
       </div>
       <div>
         <input type='tel' placeholder='avatar' onChange={event => setPhoneValue(event.target.value)} value={phoneValue} />
         <button onClick={onHandleChangePhone}>update phone</button>
+        {phoneError && <div className={s.error}>{phoneError}</div>}
       </div>
     </MainContainer>
   );
