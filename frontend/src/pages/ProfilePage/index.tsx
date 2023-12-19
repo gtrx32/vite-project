89389d62182957534a7213fcf0e964d954739d9f
gtrx32 <gtrx32@bk.ru>
2023-12-19 03:51:04 +0300

    профиль + игрульки

--- frontend/src/pages/ProfilePage/index.tsx
+++ frontend/src/pages/ProfilePage/index.tsx
@@ -4,6 +4,8 @@ import { useStateContext } from '../../context/ContextProvider';
 import { axiosClient } from '../../api/client';
 
 import s from './ProfilePage.module.scss';
+import Input from '../../components/UI/Input';
+import Button from '../../components/UI/Button';
 
 const ProfilePage = () => {
   document.title = 'Профиль';
@@ -43,19 +45,23 @@ const ProfilePage = () => {
 
   return (
     <MainContainer className={s.mainContainer}>
-      <div>{user.email}</div>
-      <div>{user.name}</div>
-      <div>{user.phone}</div>
-      <div>
-        <input type='text' placeholder='avatar' onChange={event => setAvatarValue(event.target.value)} value={avatarValue} />
-        <button onClick={onHandleChangeAvatar}>update avatar</button>
-        {avatarError && <div className={s.error}>{avatarError}</div>}
+      <div className={s.userdata}>
         <img loading='lazy' src={user?.avatar} width='200' alt='' />
+        <div className={s.userdata__name}>{user.name}</div>
+        <div className={s.userdata__email}>{user.email}</div>
+        <div className={s.userdata__phone}>{user.phone}</div>
       </div>
-      <div>
-        <input type='tel' placeholder='avatar' onChange={event => setPhoneValue(event.target.value)} value={phoneValue} />
-        <button onClick={onHandleChangePhone}>update phone</button>
-        {phoneError && <div className={s.error}>{phoneError}</div>}
+      <div className={s.settings}>
+        <div className={s.avatarsettings}>
+          <Input type='text' placeholder='Аватар' onChange={event => setAvatarValue(event.target.value)} value={avatarValue} />
+          <Button onClick={onHandleChangeAvatar}>Изменить</Button>
+          {avatarError && <div className={s.error}>{avatarError}</div>}
+        </div>
+        <div className={s.phonesettings}>
+          <Input type='tel' placeholder='Телефон' onChange={event => setPhoneValue(event.target.value)} value={phoneValue} />
+          <Button onClick={onHandleChangePhone}>Изменить</Button>
+          {phoneError && <div className={s.error}>{phoneError}</div>}
+        </div>
       </div>
     </MainContainer>
   );
